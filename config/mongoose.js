2983d9c46ed64a11f4e97edb489b91371f582238
itriku <nguyenhminhp@gmail.com>
2015-09-28 19:45:58 -0700

    mongodb

--- config/mongoose.js
+++ config/mongoose.js
@@ -1,6 +1,6 @@
 var mongoose = require('mongoose')
 var fs = require('fs')
-mongoose.connect('mongodb://localhost/SportSpot');
+mongoose.connect('mongodb://heroku_hsj8hpds:Nguyenmp93@ds051883.mongolab.com:51883/heroku_hsj8hpds');
 var models_path = __dirname + '/../server/models';
 fs.readdirSync(models_path).forEach(function(file) {
   if(file.indexOf('.js') > 0) {
