b71a8fe21fc059cdba55aee32220d602ec08f972
itriku <nguyenhminhp@gmail.com>
2015-09-29 11:51:40 -0700

    mongodb stuff

--- config/mongoose.js
+++ config/mongoose.js
@@ -1,6 +1,13 @@
 var mongoose = require('mongoose')
 var fs = require('fs')
-mongoose.connect('mongodb://heroku_hsj8hpds:Nguyenmp93@ds051883.mongolab.com:51883/heroku_hsj8hpds');
+// "mongodb://heroku_hsj8hpds:Nguyenmp93@ds051883.mongolab.com:51883/heroku_hsj8hpds" ||"mongodb://localhost/SportSpot"  
+mongoose.connect("mongodb://admin:admin1234@ds051903.mongolab.com:51903/sportspot" , function(err){
+	if(err){
+		console.log(err)
+	}else{
+		console.log('connected')
+	}
+});
 var models_path = __dirname + '/../server/models';
 fs.readdirSync(models_path).forEach(function(file) {
   if(file.indexOf('.js') > 0) {
